5d7693792001dc7f857e0d992e4b36649f125834
syw784 <66684202+syw784@users.noreply.github.com>
2020-10-01 15:15:43 -0400

    Update selectHeptad.py

--- selectHeptad.py
+++ selectHeptad.py
@@ -1,11 +1,12 @@
-#what a waste of time. for coiled coil scholar wannabes i dont know how this is not in the world wide of the magnificent web yet.
-#run pymol, file -> run script, run this, type selectHeptad XXX where X is the first residuue number wqhen u open display-sequences, becasue people dont like follow rules
-#and start with random numbers when possible. this selects heptads for you so you can look at each and individual face's spheres and detemrine contact.
-#example: selectHeptad 1
-#if nothing is selected, try display->sequences->does it start with 1? weird numbers typically happen with dimers. do selectHeptad whatever number.
-#look for A-D face which should be the hydrophobic core part. lets say face #1 is A. do renameHeptad 1, A
-
 from pymol import cmd
+
+# coiled coil pymol toolkit. would have appreciated it if something similar was searchable 2 years ago so i can actually understand coiled-coil design. F.K. my phd.
+#features:
+#1. make all 7 heptad faces into selections by typing selectHeptad 1 (or whatever the number your sequence starts with. if you have a dimer and start wth diff numbers, run it twice)
+#2. rename heptad faces into ABCDEFGs by typing renameHeptad face# face, like renameHeptad 1, A if heptad1 is A face.
+#3. color heptad faces so its more distinguishable when shown as spheres. ad is green, e is red, g is blue, cb is yellow, f is cyan.
+#4. del_disabled_obj deletes all disabled objects. i have to generate symmetry mates and get a lot of not useful objects, they make selection another level of mess on top of an already messy one; use this to mass delete them.
+#5. merge_enabled merges all enabled objects into a new object. I use this to generate a coiled-coil bundle from monomer by generating symmetry mates, then merge into a separate coiled coil object. 
 
 def pymol_select2(i):
     s = ''
@@ -27,7 +28,10 @@ def renameHeptad(i, face):
     i = int(i)
     face = faces.index(face)
     for j in range(7):
-        cmd.set_name('heptad{}'.format(i % 7), 'heptad{}'.format(faces[face % 7]))
+        try:
+            cmd.set_name('heptad{}'.format(i % 7), 'heptad{}'.format(faces[face % 7]))
+        except:
+            'okay'
         i += 1
         face += 1
     cmd.select('heptadAD', 'heptadA, heptadD')
@@ -51,6 +55,24 @@ def colorHeptad(atomic = None):
     if atomic:
         cmd.color('atomic', '(not elem C)')
 
+def del_disabled_obj():
+    r = cmd.get_object_list('enabled')
+    for i in cmd.get_object_list():
+        try:
+            r.index(i)
+        except:
+            cmd.delete(i)
+    #string += '_'
+    #for i in cmd.get_object_list():
+     #   if not i.find(string) == -1:
+      #      cmd.delete(i)
+
+def merge_enabled(name = 'merged'):
+    cmd.select('sele', 'enabled')
+    cmd.create(name, 'sele')
+    
 cmd.extend('selectHeptad', selectHeptad)
 cmd.extend('renameHeptad', renameHeptad)
 cmd.extend('colorHeptad' , colorHeptad)
+cmd.extend('del_disabled_obj', del_disabled_obj)
+cmd.extend('merge_enabled', merge_enabled)
